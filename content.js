099d918d7a43a7c351b43cfc1034aa6544741e24
Callum <callumc34@hotmail.com>
2020-10-27 12:30:06 +1300

    Added checkbox functionality

--- content.js
+++ content.js
@@ -1,29 +1,78 @@
+class deemix_dl_extension {
+
+	downloadAlbum() {
+		copyCommand("deemix -l " + window.location.href);
+	}
+
+	createAlbumButton() {
+		let button = document.createElement("input");
+		button.name = "dlAlbumButton"
+		button.value = "Download Album";
+		button.type = "button"
+		button.className = "root-0-3-1 containedPrimary-0-3-9 deemix_dl_button";
+		document.getElementsByClassName("header-creator")[0].insertAdjacentElement(
+			"afterend", button)
+		button.addEventListener("click", this.downloadAlbum);
+
+		//todo add download selected songs
+
+	}
+
+	createDownloadButton() {
+		let button = document.createElement("input");
+		button.value = "Download Selected";
+		button.type = "button"
+		button.className = "root-0-3-1 containedPrimary-0-3-9 deemix_dl_button";
+		document.getElementsByName("dlAlbumButton")[0].insertAdjacentElement(
+			"afterend", button);
+	}
+
+	addAlbum(checkbox) {
+		let songElement = checkbox.parentElement.parentElement.parentElement.children[2]
+		let songName = songElement.innerText;
+		if (checkbox.checked) {
+
+			let songUrl = songElement.children[0].children[0].href;
+
+			this.selectedSongs[songName] = songUrl;
+		} else {
+			delete this.selectedSongs[songName];
+		}
+
+
+		console.log(this.selectedSongs);
+	}
+
+
+	constructor() {
+
+		this.selectedSongs = {}
+
+		this.createAlbumButton();
+		this.createDownloadButton();
+	}
+}
+
 function copyCommand(text) {
 	navigator.clipboard.writeText(text).then(() => {
 	}, () => {});
 }
 
-function downloadAlbum() {
-	copyCommand("deemix -l " + window.location.href);
-}
+function main() {
+	var extension = new deemix_dl_extension();
 
-function createButton() {
-	let button = document.createElement("input");
-	button.value = "Download Album";
-	button.type = "button"
-	button.className = "root-0-3-1 containedPrimary-0-3-9";
-	document.getElementsByClassName("header-creator")[0].insertAdjacentElement(
-		"afterend", button)
-	button.addEventListener("click", downloadAlbum);
-
-	//todo add download selected songs
+	for (let checkbox of document.getElementsByClassName("checkbox-input")) {
+		checkbox.addEventListener("change", function () {
+			extension.addAlbum(checkbox);
+		});
+	}
 
 }
 
 var checkExist = setInterval(function() {
 	if (document.getElementsByClassName("header-creator")[0]) {
-		console.log("Running button creation...");
-		createButton()
+		console.log("Running extension...");
+		main()
 		clearInterval(checkExist)
 	}
 }, 100)
